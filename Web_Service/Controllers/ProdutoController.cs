171c09fad91fa6216aed5743944bdb10a968017f
Igor12x <igorbtenorioidi@gmail.com>
2023-03-31 19:28:22 -0300

    Adicionar arquivos de projeto.

---
+++ Web_Service/Controllers/ProdutoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace Web_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        //Os métodos para serem acessados externamente, precisam ser public
+        //Precisamos indicar o método de acesso, ou seja, se será acessado
+        //usando GET, POST, PUT (editar) OU DELETE
+        //É possível retornar qualquer tipo de dados, isto é, int, double,
+        //List, etc ou usar a interface IActionResult para deixar no padrão
+        //de webservice
+
+        [HttpPost]
+        //configurar uma rota espefícica acrecentar [Route("api/[controller]/cadastrar")] por exemplo.
+        public IActionResult cadastrar([FromBody] /*FromBody - copor da requisão - não aparece na URL - ´médoto POST*/ Produto p){
+            MySqlConnection conexao = new MySqlConnection(
+                "Sever= ;Database= ; User= ;password=  ");
+      
+            MySqlCommand sql = new MySqlCommand(
+                "INSERT  INTO produto VALUES(@id, @nome, @preco, @qtde)", conexao);
+            sql.Parameters.AddWithValue("@id", p.id);
+            sql.Parameters.AddWithValue("@nome", p.nome);
+            sql.Parameters.AddWithValue("@preco", p.preco);
+            sql.Parameters.AddWithValue("@qtde", p.quantidade);
+            conexao.Open();
+            if (sql.ExecuteNonQuery() != 0)
+            {
+                //usando a interface IActionResult para retornar no padrão webservice
+                //o método Ok retorna código 200 (sucesso)
+                conexao.Close();
+                return Ok(new { mensagem = "Cadastrado"});
+            }
+            else
+            {
+                conexao.Clone();
+                return NoContent(); //Código 204 (sem conteúdo)
+            }
+        }
+    }
+}
---
+++ Web_Service/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Service
+{
+    public class Produto
+    {
+        //Atributos - Atralho: prop e apertar Tab 2x
+        public int id { get; set; }
+        public String nome { get; set; }
+        public double preco { get; set; }
+        public int quantidade { get; set; }
+
+        //Construtor - Atalho: Ctrl .
+
+        public Produto(int id, string nome, double preco, int quantidade)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+        public Produto() { }
+    }
+}
